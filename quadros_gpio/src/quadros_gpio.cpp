11d3b4745f9f59fd95d16f1180e2d88b8eded6c0
Kelvin <nguyenleminh1002@gmail.com>
2025-06-04 11:01:50 +1000

    Update MotorSpeed subscription topic and enhance motor control logic for armed state

--- quadros_gpio/src/quadros_gpio.cpp
+++ quadros_gpio/src/quadros_gpio.cpp
@@ -23,7 +23,7 @@ public:
         }
 
         sub_ = this->create_subscription<quadros_gpio::msg::MotorSpeed>(
-            "MotorSpeed", 10,
+            "/quadros/set/motors", 10,
             std::bind(&MotorPwmNode::motor_callback, this, std::placeholders::_1)
         );
 
@@ -39,7 +39,7 @@ public:
     }
 
 private:
-    void motor_callback(const quadros_gpio::msg::MotorSpeed::SharedPtr msg)
+    void motor_callback(const quadros_gpio::msg::MotorSpeed::SharedPtr msg) // This function tracks the motor speeds
     {
         std::array<float, 4> percentages = {
             msg->motor_speed_1,
@@ -47,13 +47,21 @@ private:
             msg->motor_speed_3,
             msg->motor_speed_4
         };
-
-        for (size_t i = 0; i < percentages.size(); ++i) {
-            float percent = std::clamp(percentages[i], 0.0f, 100.0f);
-            int microseconds = static_cast<int>(1000 + (percent / 100.0f) * 1000); // 1000–2000 µs
-            gpioServo(motor_pins_[i], microseconds);
-            RCLCPP_INFO(this->get_logger(), "ESC[%ld] set to %.1f%% => %d µs", i+1, percent, microseconds);
+        bool armed = msg->armed;
+        if (armed) {
+            for (size_t i = 0; i < percentages.size(); ++i) {
+                float percent = std::clamp(percentages[i], 0.0f, 100.0f);
+                int microseconds = static_cast<int>(1000 + (percent / 100.0f) * 1000); // 1000–2000 µs
+                gpioServo(motor_pins_[i], microseconds);
+                RCLCPP_INFO(this->get_logger(), "ESC[%ld] set to %.1f%% => %d µs", i+1, percent, microseconds);
+            }
+        } else {
+            RCLCPP_INFO(this->get_logger(), "Motors NOT armed, setting to 0%");
+            for (int pin : motor_pins_) {
+                gpioServo(pin, 0); // Stop signal
+            }
         }
+
     }
 
     std::array<int, 4> motor_pins_;
